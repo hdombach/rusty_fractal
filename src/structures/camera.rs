70622f68a5d317b25045917148521853936d4b39
Hezekiah Dombach <hezekiahdombach@gmail.com>
2022-10-13 21:41:47 -0400

    added camera

--- src/structures/camera.rs
+++ src/structures/camera.rs
@@ -1,3 +1,43 @@
-pub struct Camera {
+use glam::*;
 
+pub struct Camera {
+   position: Vec3,
+   rotation: Quat,
+}
+
+impl Camera {
+    pub fn new() -> Self {
+        Self {
+            position: Vec3::default(),
+            rotation: Quat::default(),
+        }
+    }
+
+    pub fn get_position(&self) -> Vec3 {
+        self.position
+    }
+    pub fn set_position(&mut self, value: Vec3) {
+        self.position = value;
+    }
+    pub fn change_position(&mut self, value: Vec3) {
+        self.position += value;
+    }
+
+    pub fn get_rotation_quat(&self) -> Quat {
+        self.rotation
+    }
+    pub fn set_rotation_quat(&mut self, value: Quat) {
+        self.rotation = value;
+    }
+    pub fn rotate_camera(&mut self, value: Quat) {
+        self.rotation *= value;
+    }
+
+    pub fn get_rotation_matrix(&self) -> Mat4 {
+        Mat4::from_quat(self.rotation)
+    }
+
+    pub fn get_translation_matrix(&self) -> Mat4 {
+        Mat4::from_quat(self.rotation) * Mat4::from_translation(self.position)
+    }
 }
--- src/util/mod.rs
+++ src/util/mod.rs
@@ -1,5 +1,1 @@
 pub mod ref_dict;
-pub mod vec2;
-pub mod vec3;
-pub mod vec4;
-pub mod mat4x4;
